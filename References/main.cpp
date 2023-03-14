ffb952fcdbf56e2b5a7b843bc9e900271f091ff9
Б - 5 < - 5@DESKTOP-EOJTJK3>
2023-03-14 21:08:55 +0300

    Функция, возвращающая ссылку

--- References/main.cpp
+++ References/main.cpp
@@ -15,17 +15,35 @@ void p_swap(T* a, T* b) {
 	*b = tmp;
 }
 
-template <typename T> // 
+template <typename T> // 3-я - копия std::swap()
 void ref_swap(T& a, T& b) {
 	T tmp = a;
 	a = b;
 	b = tmp;
 }
 
+int MX[10][2];
+int& func(int index) {
+	return MX[index][1];
+}
+
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int n, m;
 	
+	std::cout << "Ввод в массив.\n";
+	for (int i{}; i < 10; i++) {
+		std::cout << "Введите " << i + 1 << "-й элемент -> ";
+		std::cin >> MX[i][0];
+		func(i) = MX[i][0];
+	}
+
+	std::cout << "Итоговый массив:\n";
+	for (int i{}; i < 10; i++)
+		std::cout << MX[i][0] << "\t" << func(i) << "\n";
+
+
+
 	// Ссылки - параметры функций
 	/*
 	n = 10; m = 15;
